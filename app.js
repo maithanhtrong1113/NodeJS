95c9d5f1dbde08c67cd0971ca7f86ac83618a5d2
maithanhtrong1113 <maithanhtrong1113@gmail.com>
2022-09-29 02:19:22 +0700

    staticFile router views

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
+const path = require("path");
 const express = require("express");
-
 const bodyParser = require("body-parser");
-
-app.use(bodyParser.urlencoded());
-
+const adminRoutes = require("./routes/admin");
 const app = express();
 
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello</h1>");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "views", "shop.html"));
 });
+
+app.use("/admin", adminRoutes);
+
+app.use((req, res) => {
+  res.status(404).send("<h1>Page Not Found</h1>");
+});
+
 app.listen(3000);
