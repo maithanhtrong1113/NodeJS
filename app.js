28b4a77b3c54a413274506d532003f530d88be91
maithanhtrong1113 <maithanhtrong1113@gmail.com>
2022-09-29 04:07:36 +0700

    ejs init

--- app.js
+++ app.js
@@ -4,8 +4,15 @@ const bodyParser = require("body-parser");
 const adminRoutes = require("./routes/admin");
 const app = express();
 
+//  Templating Engines
+app.set("view engine", "ejs");
+app.set("views", "views");
+// config bodyParser
 app.use(bodyParser.urlencoded({ extended: false }));
+// config bodyParser static file
 app.use(express.static(path.join(__dirname, "public")));
+
+// show view
 app.get("/", (req, res, next) => {
   res.sendFile(path.join(__dirname, "views", "shop.html"));
 });
